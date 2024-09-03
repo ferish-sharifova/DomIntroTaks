5d5fa501444e29c87813d5b5b05b5edd98e1f677
ferish-sharifova <farizabalabekoba199505@gmail.com>
2024-09-04 00:39:36 +0400

    dom tapshiriqlari

---
+++ main.js
@@ -0,0 +1,46 @@
+const myCard=document.querySelector(".card");
+const myButton=document.querySelector("button");
+const cardHeader=document.querySelector(".card-header");
+const myh5=document.querySelector("h5")
+const p=document.querySelector("p")
+// Task1
+// myCard.classList="card text-bg-info mb-3" ;
+
+// Task2 
+// function  myfunction(){
+//     myCard.classList="card text-bg-info mb-3" 
+// }
+// myButton.addEventListener("click",myfunction) 
+
+// Task3
+// cardHeader.style.color="red"
+
+// Task4
+// myCard.style.maxWidth="30em"
+
+// task5
+
+// function changeText(){
+//     myh5.innerHTML="yazini deyisdim"
+// }
+// myButton.addEventListener("click",changeText)
+
+// task6
+// function changemyButton(){
+//  myButton.style.backgroundColor="green"
+// }
+// myButton.addEventListener("click",changemyButton) 
+
+
+
+
+
+// Task1
+// myh5.innerHTML="coders"
+
+// task2
+// cardHeader.innerHTML+='<span class="badge text-bg-secondary">4</span>'
+
+// task3
+
+// p.innerHTML="lorem ipsum"
